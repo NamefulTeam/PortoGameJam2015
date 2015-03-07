522004771f1bb0568183d9a92569a030c6124437
JPonte <joaopcponte@gmail.com>
2015-03-07 16:26:31 +0000

    Watcher.

---
+++ watcher.lua
@@ -0,0 +1,35 @@
+directions = require 'directions'
+
+local function exports(x, y, direction)
+	assert(directions.is_direction(direction))
+
+	local instance = {}
+
+	instance.x = x
+	instance.y = y
+	instance.direction = direction
+
+	instance.image = love.graphics.newImage('placeholders/watcher.png')
+	instance.quad = love.graphics.newQuad(0, 0, 32, 32, 32, 32)
+
+	function instance:update(grid)
+		local next_x = self.x + directions.get_x_diff(self.direction)
+		local next_y = self.y + directions.get_y_diff(self.direction)
+
+		grid:set_space_at(self.x, self.y, true)
+
+		self.x = next_x
+		self.y = next_y
+	end
+
+	function instance:draw(offset_x, offset_y)
+		local actual_x = offset_x + self.x * 32
+		local actual_y = offset_y + self.y * 32
+
+		love.graphics.draw(self.image, self.quad, actual_x - 16, actual_y - 16, directions.get_angle(self.direction), 1, 1, 16, 16)
+	end
+
+	return instance
+end
+
+return exports
