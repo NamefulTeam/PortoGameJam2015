0248b0d7123c97c9d28ececa0617297f77e54384
broclan <afrosa2@gmail.com>
2015-03-07 19:17:57 +0000

    gameOverBackground added

--- game_over_view.lua
+++ game_over_view.lua
@@ -2,10 +2,13 @@ function exports()
 	local instance = {}
 
 	local background = love.graphics.newImage('background/background_light.png')
+	local gameOverImage = love.graphics.newImage('background/gameOverBackground.png')
 
 	function instance:draw()
-		love.graphics.setColor(50,50,50)
+		love.graphics.setColor(80,80,80)
 		love.graphics.draw(background, 0, 0)
+		love.graphics.setColor(255,255,255)
+		love.graphics.draw(gameOverImage, 350, 150)
 	end
 
 	function instance:update()
