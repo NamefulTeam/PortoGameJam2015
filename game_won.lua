62b3ba0b86bcb152ceeb09f395ce88bbf2418963
broclan <afrosa2@gmail.com>
2015-03-08 12:24:09 +0000

    game_won with better looking

--- game_won.lua
+++ game_won.lua
@@ -6,22 +6,22 @@ numberOfRounds = 5
 function exports()
 	local instance = {}
 
-	local background = love.graphics.newImage('background/title_background.png')
+	local background = love.graphics.newImage('background/background_light.png')
 	local gameOverImage = love.graphics.newImage('background/gameWonBackground.png')
-	local mainMenuButton = love.graphics.newImage('placeholders/main_menu.png')
+	local mainMenuButton = love.graphics.newImage('placeholders/main_menu2.png')
 
 	function instance:draw()
-		love.graphics.setColor(80,80,80)
+		love.graphics.setColor(0,10,145)
 		love.graphics.draw(background, 0, 0)
 		love.graphics.setColor(255,255,255)
 		love.graphics.draw(gameOverImage, 350, 150)
-		love.graphics.draw(mainMenuButton, 550, 550)
+		love.graphics.draw(mainMenuButton, 550, 450)
 	end
 
 	function instance:update()
 		mouse_x, mouse_y = love.mouse.getPosition()
 
-		if love.mouse.isDown("l") and mouse_x > 550 and mouse_x <= 750 and mouse_y > 550 and mouse_y <= 700 then
+		if love.mouse.isDown("l") and mouse_x > 550 and mouse_x <= 800 and mouse_y > 450 and mouse_y <= 510 then
 			initial_menu_view = require 'initial_menu_view'
 			active_screen.set(initial_menu_view())
 			return
