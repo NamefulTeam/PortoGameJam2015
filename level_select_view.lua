e64a014d683f57aa1cc7d0daa639fb7372f55a67
Luis Reis <luiscubal@gmail.com>
2015-03-08 13:07:23 +0000

    Level Select View implemented
    Could still use some work

--- level_select_view.lua
+++ level_select_view.lua
@@ -12,12 +12,24 @@ function exports()
 	local backWidth = 300
 	local backHeight = 100
 
+	local levelButton = love.graphics.newImage('placeholders/watcher.png')
+
+	local levelStartX = 50
+	local levelStartY = 100
+	local levels = level_list.get_levels()
+
 	function instance:update()
 		local mouse_x, mouse_y = love.mouse.getPosition()
 		
 		if love.mouse.isDown('l') then
 			if mouse_x >= backX and mouse_y >= backY and mouse_x < backX + backWidth and mouse_y < backY + backHeight then
 				active_screen.set(require('initial_menu_view')())
+			end
+
+			local x = levelStartX
+			local y = levelStartY
+			for i = 1, #levels do
+				active_screen.set(game_view(levels[i]))
 			end
 		end
 	end
@@ -27,6 +39,12 @@ function exports()
 		love.graphics.rectangle('fill', 0, 0, 1280, 720)
 
 		love.graphics.draw(backButton, backX, backY)
+
+		local x = levelStartX
+		local y = levelStartY
+		for i = 1, #levels do
+			love.graphics.draw(levelButton, x, y)
+		end
 	end
 
 	return instance
