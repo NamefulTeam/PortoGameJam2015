98513be5114337d26b8febd55ffe1a99a2b6ac1c
JPonte <joaopcponte@gmail.com>
2015-03-08 10:18:34 +0000

    Placeables highlight.

--- levels/test_level.lua
+++ levels/test_level.lua
@@ -1,9 +1,15 @@
+placeable_utils = require 'placeables'
 exports = {}
 
 function exports.setup(grid_state)
 	for watcherI=1, 5 do
 		grid_state:add_object(watcher(math.random(1, grid_state.width), math.random(1, grid_state.height), directions.DOWN))
 	end
+
+	local placeable = {}
+	placeable_utils.add_placeableblock(placeable, 2,2,4,3)
+	placeable_utils.add_placeableblock(placeable, 5,5,2,4)
+	grid_state.placeable = placeable_utils.get_single_cell_list(placeable)
 end
 
 exports.number_of_rounds = 5
