9167808020b69bc9db4174be024f1115ac914dee
broclan <afrosa2@gmail.com>
2015-03-08 12:49:56 +0000

    retry and menu working in game_view

--- game_view.lua
+++ game_view.lua
@@ -257,6 +257,13 @@ function exports(level_description)
 					end
 				elseif mouse_x > goButtonX and mouse_x <= goButtonX + goButtonWidth and mouse_y > goButtonY and mouse_y <= goButtonY + goButtonHeight and not lastFrameMouseClicked then
 					processGoButtonClicked(self.grid_state, self.player_state)
+				elseif mouse_x > goButtonX and mouse_x <= goButtonX + goButtonWidth and mouse_y > retryButtonY and mouse_y <= retryButtonY + goButtonHeight and not lastFrameMouseClicked then
+					active_screen.set(game_view(level_description))
+					return
+				elseif mouse_x > goButtonX and mouse_x <= goButtonX + goButtonWidth and mouse_y > menuButtonY and mouse_y <= menuButtonY + goButtonHeight and not lastFrameMouseClicked then
+					initial_menu_view = require 'initial_menu_view'
+					active_screen.set(initial_menu_view())
+					return
 				end
 			end
 		else
