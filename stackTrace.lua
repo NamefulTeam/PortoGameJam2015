5ef05edfa320489cf11b4738e55009de4849d878
JPonte <joaopcponte@gmail.com>
2015-03-08 09:25:40 +0000

    Mouse over color fix.

--- stackTrace.lua
+++ stackTrace.lua
@@ -1,8 +1,6 @@
 exports = {}
 function exports.draw_stack(grid, x, y, mouse_x, mouse_y, offset_x, offset_y, current)
 	love.graphics.setColor(255,0,0)
-	--love.graphics.rectangle("fill",x,y, 250,20)
-	--love.graphics.setColor(0,0,0)
 	love.graphics.print('Unhandled InternalErrorException:', x+5, y+5)
 	local index = 1
 	local current_object = grid.first_object
@@ -13,11 +11,11 @@ function exports.draw_stack(grid, x, y, mouse_x, mouse_y, offset_x, offset_y, cu
 			love.graphics.setColor(0,0,0)
 			love.graphics.rectangle("line",x,y+index*25, 235,20)
 			
-		if current_object.type == 'glider' then
-			love.graphics.setColor(64,128,64)
-		else
-			love.graphics.setColor(128,128,255)
-		end
+			if current_object.type == 'glider' then
+				love.graphics.setColor(102,255,102)
+			else
+				love.graphics.setColor(102,255,255)
+			end
 
 			love.graphics.rectangle("fill", current_object.x*32+offset_x-radius, current_object.y*32+offset_y-radius, radius, radius);
 		end
